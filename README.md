c518a8bdaba3b2277084ca2af83352db356faeeb
Jamu Kakar <jkakar@kakar.ca>
2012-03-09 18:04:36 -0800

    - Add logic to detect changes in tabs. - Add (broken) logic to detect newly loaded pages. - Add a label to show the URL of the current page, for debugging   purposes. - Add a 'Handling page and tab changes' section to the README.md file   to describe the various conditions in which the extension must   update itself to match the current page.

--- README.md
+++ README.md
@@ -4,19 +4,20 @@ Infomanic is a Firefox extension for Fluidinfo.
 Introduction
 ------------
 
-Infomaniac is a Firefox extension that makes it easy for a user to use
-Fluidinfo while they're surfing the web.  The extension adds a sidebar
-to the browser and, as such, must be XUL-based.
+Infomaniac is a Firefox extension that makes it easy to follow web
+pages, see the info other people have stored and to add one's own info
+about them.  The extension adds a sidebar to the browser and, as such,
+must be XUL-based.
 
 
-Development
------------
+Development and testing
+-----------------------
 
 Follow the instructions here to prepare a development profile and
 tweak some settings to get information about errors when the extension
 runs:
 
-https://developer.mozilla.org/en/Setting_up_extension_development_environment
+  https://developer.mozilla.org/en/Setting_up_extension_development_environment
 
 When that's done put a file in `~/.mozilla/firefox/<profile>/extensions`
 called `infomaniac@fluidinfo.com` with a single line containing the
@@ -24,3 +25,30 @@ path to this repository.  Start Firefox and hit `Ctrl-Shift-f` to see
 the extension in action:
 
     /usr/bin/firefox -no-remote -P dev
+
+
+Handling page and tab changes
+-----------------------------
+
+The extension needs to detect a few several kinds of state changes and
+update itself to ensure that the content it displays always matches
+the current page:
+
+- A new tab is opened showing the empty `about:blank` page.
+- A new tab is opened showing a page that can be followed.
+- The active tab is switched from one to another.
+- The page in the active tab isn't loaded yet.
+- The page in the active tab finishes loading.
+- The page in the active tab is changed.
+- The page in an inactive tab has changed since the last time the
+  extension updated itself (say because of an automatic refresh or
+  because a page was loading slowly and the user switched away while
+  it finished).
+
+As you can see many things can happen that must result in the
+extension updating its state.  The way it does this is by listening
+for and responding to a combination of events, which are described on
+these pages:
+
+  https://developer.mozilla.org/en/Code_snippets/Tabbed_browser
+  https://developer.mozilla.org/en/Code_snippets/On_page_load
--- chrome/content/infomaniac.js
+++ chrome/content/infomaniac.js
@@ -12,21 +12,72 @@ var infomaniac = {
 
     // Triggered when the window's "load" event is fired.
     onLoad: function() {
+        var mainWindow = infomaniac.getMainWindow();
+        var label = window.document.getElementById("current-url");
+        label.value = mainWindow.gBrowser.contentDocument.location.href;
+
+        infomaniac.tabs.onLoad();
     },
 
     // Triggered when the window's "unload" event is fired.
     onUnload: function() {
+        infomaniac.tabs.onUnload();
+    },
+
+    // Logic related to tab behaviour.
+    tabs: {
+        // Register event listeners to detect tab selection changes.
+        // Triggered when the extension is loaded and ready to use.
+        onLoad: function() {
+            var mainWindow = infomaniac.getMainWindow();
+            var container = mainWindow.gBrowser.tabContainer;
+            container.addEventListener("TabSelect", infomaniac.tabs.onSelect,
+                                       false);
+            mainWindow.gBrowser.addEventListener("load",
+                                                 infomaniac.tabs.onPageLoad,
+                                                 false);
+        },
+
+        // Unregister event listeners to stop detecting tab selection
+        // changes.  Triggered when the extension is unloaded.
+        onUnload: function() {
+            var mainWindow = infomaniac.getMainWindow();
+            var container = mainWindow.gBrowser.tabContainer;
+            container.removeEventListener("TabSelect",
+                                          infomaniac.tabs.onSelect, false);
+        },
+
+        // Determine if the sidebar needs to be updated to reflect a new page.
+        // Triggered when a new page is loaded.
+        onPageLoad: function(evt) {
+            var document = evt.originalTarget;
+            alert(document);
+            if (document.nodeName == "#document") {
+                alert("page-load");
+            }
+        },
+
+        // Change the label that shows the current URL, to demonstrate
+        // that we can detect tab selection changes.
+        onSelect: function() {
+            var mainWindow = infomaniac.getMainWindow();
+            var label = window.document.getElementById("current-url");
+            label.value = mainWindow.gBrowser.contentDocument.location.href;
+        }
     },
 
     // Logic related to the behaviour of the "Follow" button.
     follow: {
-        // Triggered when the "Follow" button is pressed.
+        // Toggle the follow state of the current document.  Triggered
+        // when the "Follow" button is pressed.
         onClick: function() {
             var button = window.document.getElementById("follow-button");
             button.label = button.label == "Follow" ? "Following" : "Follow";
         },
 
-        // Triggered when the "Follow" button is hovered upon.
+        // Change the name of the "Following" button to "Unfollow", to
+        // make it clear what clicking it will do.  Triggered when the
+        // "Follow" button is hovered upon.
         onMouseOver: function() {
             var button = window.document.getElementById("follow-button");
             if (button.label == "Following") {
@@ -34,7 +85,9 @@ var infomaniac = {
             }
         },
 
-        // Triggered when the mouse is moved away from the "Follow" button.
+        // Change the "Unfollow" label back to "Following" if the
+        // current document is followed.  Triggered when the mouse is
+        // moved away from the "Follow" button.
         onMouseOut: function() {
             var button = window.document.getElementById("follow-button");
             if (button.label == "Unfollow") {
